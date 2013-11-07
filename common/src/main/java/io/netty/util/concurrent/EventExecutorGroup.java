95fcf0205467fb475a5d169e50dccf5a5a4b4bc0
Trustin Lee <t@motd.kr>
2013-11-07 17:47:40 +0900

    Better docs

--- common/src/main/java/io/netty/util/concurrent/EventExecutorGroup.java
+++ common/src/main/java/io/netty/util/concurrent/EventExecutorGroup.java
@@ -30,8 +30,8 @@ import java.util.concurrent.TimeUnit;
 public interface EventExecutorGroup extends ScheduledExecutorService {
 
     /**
-     * Returns {@code true} if and only if this executor was started to be
-     * {@linkplain #shutdownGracefully() shut down gracefuclly} or was {@linkplain #isShutdown() shut down}.
+     * Returns {@code true} if and only if all {@link EventExecutor}s managed by this {@link EventExecutorGroup}
+     * are being {@linkplain #shutdownGracefully() shut down gracefuclly} or was {@linkplain #isShutdown() shut down}.
      */
     boolean isShuttingDown();
 
@@ -59,7 +59,8 @@ public interface EventExecutorGroup extends ScheduledExecutorService {
     Future<?> shutdownGracefully(long quietPeriod, long timeout, TimeUnit unit);
 
     /**
-     * Returns the {@link Future} which is notified when this executor has been terminated.
+     * Returns the {@link Future} which is notified when all {@link EventExecutor}s managed by this
+     * {@link EventExecutorGroup} have been terminated.
      */
     Future<?> terminationFuture();
 
@@ -78,12 +79,12 @@ public interface EventExecutorGroup extends ScheduledExecutorService {
     List<Runnable> shutdownNow();
 
     /**
-     * Returns one of the executors that belong to this group.
+     * Returns one of the {@link EventExecutor}s managed by this {@link EventExecutorGroup}.
      */
     EventExecutor next();
 
     /**
-     * Returns the unmodifiable set of executors managed by this group.
+     * Returns the unmodifiable set of {@link EventExecutor}s managed by this {@link EventExecutorGroup}.
      */
     <E extends EventExecutor> Set<E> children();
 
